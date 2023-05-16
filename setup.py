6c1a204659867327a0e64897d43e05e3b7e21f2c
Zeus Chiu <zeuscsc@gmail.com>
2023-05-16 15:38:44 +0800

    fix setup bugs

--- setup.py
+++ setup.py
@@ -5,7 +5,10 @@ setup(
     version = '0.1.0',
     url = 'https://github.com/zeuscsc/auto_obsidian.git',
     description = 'Auto Generate Obsidian from your Exported Toby History',
-    packages = find_packages(include=["*"]),
+    packages = find_packages(),
+    py_modules=["*"],
+    author="ZeusChiu",
+    include_package_data=True,
     install_requires = [
         "openai",
         "python-dotenv",
