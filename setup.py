65f6cb7a62a2391891b08d5c01191c4a27e0a37b
Zeus Chiu <zeuscsc@gmail.com>
2023-05-16 15:48:27 +0800

    package_dir

--- setup.py
+++ setup.py
@@ -6,6 +6,12 @@ setup(
     url = 'https://github.com/zeuscsc/auto_obsidian.git',
     description = 'Auto Generate Obsidian from your Exported Toby History',
     packages = ["articles2notes","snapshots4notes","yt2txt","ytdl","obsidian_notes_generator","video2snapshots"],
+    package_dir={"articles2notes":"articles2notes",
+                 "snapshots4notes":"snapshots4notes",
+                 "yt2txt":"yt2txt",
+                 "ytdl":"ytdl",
+                 "obsidian_notes_generator":"obsidian_notes_generator",
+                 "video2snapshots":"video2snapshots"},
     author="ZeusChiu",
     include_package_data=True,
     install_requires = [
