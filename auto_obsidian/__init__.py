fb4056411b37e1d757dd21d6b14c426d8e80dfe7
Zeus Chiu <zeuscsc@gmail.com>
2023-05-16 16:25:19 +0800

    sys.path.append("auto_obsidian")

--- auto_obsidian/__init__.py
+++ auto_obsidian/__init__.py
@@ -1,9 +1,4 @@
-import gpt as gpt
-import ytdl as ytdl
-import yt2txt as yt2txt
-import video2snapshots as video2snapshots
-import articles2notes as articles2notes
-import snapshots4notes as snapshots4notes
-import obsidian_notes_generator as obsidian_notes_generator
+import sys
+sys.path.append("auto_obsidian")
 
 PACKAGE_NAME = "auto_obsidian"
