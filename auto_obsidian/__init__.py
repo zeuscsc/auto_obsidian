7b68ab124f57e4211492fabe9c8ed4901ced7986
Zeus Chiu <zeuscsc@gmail.com>
2023-05-16 15:58:36 +0800

    __init__

---
+++ auto_obsidian/__init__.py
@@ -0,0 +1,8 @@
+from auto_obsidian import articles2notes
+from auto_obsidian import gpt
+from auto_obsidian import obsidian_notes_generator
+from auto_obsidian import snapshots4notes
+from auto_obsidian import yt2txt
+from auto_obsidian import ytdl
+
+PACKAGE_NAME = "auto_obsidian"
